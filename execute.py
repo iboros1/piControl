edbaa86e5fa49cf581337fd4722d02581413f6fa
iboros1 <iborosppc@gmail.com>
2017-05-16 20:46:59 +0300

    Display on of

--- execute.py
+++ execute.py
@@ -1,9 +1,25 @@
-from subprocess import call,run
+import subprocess
 import os
+# command to monitor the screensaver
+# MONITOR = "xscreensaver-command"
+# MONITOR_ARGS = [MONITOR, "-watch"]
 
-def hdmi():
-    run(['/opt/vc/bin/tvservice', '-o'])
+# commands to turn the screen on and off
+# update control to  have the value vcgencmd
+CONTROL = "vcgencmd"
+DISABLE_DISPLAY = [CONTROL, "display_power", "0"]
+ENABLE_DISPLAY = [CONTROL, "display_power", "1"]
+DEFAULT = ['ON', 'green']
 
 
+def hdmi(DEFAULT):
+    if DEFAULT == ['ON', 'green']:
+        subprocess.call(DISABLE_DISPLAY)
+        DEFAULT[0], DEFAULT[1] = 'OFF', 'red'
+    else:
+        subprocess.call(ENABLE_DISPLAY)
+        DEFAULT[0], DEFAULT[1] = 'ON', 'green'
+    return DEFAULT
 
-    hdmi()
+hdmi(DEFAULT)
+
