aa80ea42c8095dec3cc62ea7e8f383bf8e9850d2
iboros1 <iborosppc@gmail.com>
2017-05-30 20:53:56 +0300

    Display on of

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3.5
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from execute import DEFAULT, hdmi
 
 app = Flask(__name__)
 #app.run(host='0.0.0.0', port='5002')
 #wsgi_app = app.wsgi_app
 
-@app.route('/')
-def hello_world():
+
+@app.route('/', methods=['GET', 'POST'])
+def tv_status():
     col = DEFAULT[1]
     default = DEFAULT[0]
-    return render_template('index.html', state=default, color=col)
-    hdmi(DEFAULT)
-
-
-
+    if request.method == 'GET':
+        return render_template('index.html', state=default, color=col)
+    elif request.method == 'POST':
+        hdmi(DEFAULT)
+        return render_template('index.html', state=default, color=col)
 
 if __name__ == "__main__":
-    hello_world()
+    tv_status()
